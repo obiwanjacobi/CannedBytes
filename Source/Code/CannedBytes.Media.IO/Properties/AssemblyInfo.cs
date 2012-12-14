fcb50a1084c624eea3b8dc07f5cd3ff9f62ea09f
Marc Jacobi <obiwanjacobi@hotmail.com>
2012-12-14 07:07:03 +0000

    Added signing and assembly info.

--- Source/Code/CannedBytes.Media.IO/Properties/AssemblyInfo.cs
+++ Source/Code/CannedBytes.Media.IO/Properties/AssemblyInfo.cs
@@ -6,33 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CannedBytes.Media.IO")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CannedBytes.Media.IO")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDescription("Media File IO")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("64bed8a9-fcb3-42c3-8c4c-f464826ec92a")]
-
-[assembly: CLSCompliant(true)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
