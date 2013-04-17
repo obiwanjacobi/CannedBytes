cd9ed6f4db37c09b4eba9f5716336c072c9c1efe
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2013-04-17 07:22:50 +0000

    Small build script changes.

--- Source/Build/BuildAndPackageRTW.cmd
+++ Source/Build/BuildAndPackageRTW.cmd
@@ -1,7 +1,6 @@
 CALL CreatePackagesDir.cmd
 
 cd Code
-CALL CleanAll.cmd
 CALL BuildAll.cmd
 CALL PackageCopyAll.cmd
 cd ..
--- Source/Build/Code/BuildAll.cmd
+++ Source/Build/Code/BuildAll.cmd
@@ -1,4 +1,2 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-
-MSBUILD /p:Configuration=Debug ..\..\Code\CannedBytes.sln
-MSBUILD /p:Configuration=Release ..\..\Code\CannedBytes.sln
+CALL Build Debug
+CALL Build Release
