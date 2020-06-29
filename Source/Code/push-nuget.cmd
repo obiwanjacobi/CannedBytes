c532756c83093b963f91f8301de8a76493b7da66
Marc Jacobi <marc.jacobi@macaw.nl>
2020-06-29 10:58:59 +0200

    add nuget

---
+++ Source/Code/push-nuget.cmd
@@ -0,0 +1,3 @@
+nuget push CannedBytes\bin\Release\CannedBytes.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Media\bin\Release\CannedBytes.Media.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Media.IO\bin\Release\CannedBytes.Media.IO.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
