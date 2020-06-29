be3f9b3e664397e8744432742a78d2ab0685fb9d
Marc Jacobi <marc.jacobi@macaw.nl>
2020-06-29 11:05:29 +0200

    publish 2.0.1

--- Source/Code/push-nuget.cmd
+++ Source/Code/push-nuget.cmd
@@ -1,3 +1,5 @@
-nuget push CannedBytes\bin\Release\CannedBytes.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Media\bin\Release\CannedBytes.Media.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
-nuget push CannedBytes.Media.IO\bin\Release\CannedBytes.Media.IO.2.0.0.nupkg -src https://api.nuget.org/v3/index.json
+msbuild CannedBytes.sln /t:Build /p:Configuration=Release
+
+nuget push CannedBytes\bin\Release\CannedBytes.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Media\bin\Release\CannedBytes.Media.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
+nuget push CannedBytes.Media.IO\bin\Release\CannedBytes.Media.IO.2.0.1.nupkg -src https://api.nuget.org/v3/index.json
